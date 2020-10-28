5f9bf12ecfc62444e216a27fc9423bf09b610a3f
shishuhua <shuhua@mgtv.com>
2020-10-28 15:30:39 +0800

    feat：swagger-ui增强

--- swagger-example/src/main/java/com/spring/boot/example/controller/LoginController.java
+++ swagger-example/src/main/java/com/spring/boot/example/controller/LoginController.java
@@ -5,28 +5,21 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 登录控制器
  *
  * @author huaijin
  */
-@Api(value = "LoginController", description = "登录控制器")
+@Api(tags = "注册登录", description = "登录鉴权")
 @RestController
 public class LoginController {
 
-
     @ApiOperation(value = "用户登录", notes = "登录接口")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "userVo", value = "用户信息",
-                    required = true, dataType = "UserVo"),
-    })
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    @PostMapping(value = "/login")
     public String login(@RequestBody UserVo userVo) {
         return "ok";
     }
+
 }
